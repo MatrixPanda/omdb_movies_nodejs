e1e6e841d3236f5a97d0b2440672b341b132a1b3
MatrixPanda <zehan11s@hotmail.com>
2020-03-20 06:19:18 -0400

    server is up and running

--- nodejs_api.js
+++ nodejs_api.js
@@ -1,3 +1,82 @@
-window.onload = function() {
-    
-}
+let express = require('express');
+let app = express();
+// let session = require('express-session');
+// let uuid = require('uuid/v1');
+let bodyParser = require('body-parser');
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('src'));
+
+
+var showtimes = [
+      {
+         id: 'tt0059742',
+         detailsURL: '/movieDetails?id=tt0059742',
+         title: 'The Sound of Music',
+         times: ['7:15pm', '8:40pm']
+      },
+      {
+         id: 'tt0059113',
+         detailsURL: '/movieDetails?id=tt0059113',
+         title: 'Doctor Zhivago',
+         times: ['7:35pm', '8:10pm', '9:05pm']
+      },
+      {
+         id: 'tt0059243',
+         detailsURL: '/movieDetails?id=tt0059243',
+         title: 'The Great Race',
+         times: ['6:45pm', '9:15pm']
+      },
+      {
+         id: 'tt0059578',
+         detailsURL: '/movieDetails?id=tt0059578',
+         title: 'For a Few Dollars More',
+         times: ['8:05pm', '9:40pm']
+      },
+      {
+         id: 'tt0059661',
+         detailsURL: '/movieDetails?id=tt0059661',
+         title: 'The Rounders',
+         times: ['4:10pm']
+      },
+      {
+         id: 'tt0059800',
+         detailsURL: '/movieDetails?id=tt0059800',
+         title: 'Thunderball',
+         times: ['8:00pm']
+      }
+   ];
+
+// var info = JSON.stringify(showtimes);
+
+app.get('/showtimes', (request, response) => { response.send(showtimes); });
+// app.get('/showtimes', (request,response) => {
+//     response.sendFile(__dirname + '/src/showtimes.html');
+// });
+
+
+app.set('port', 3001);
+app.listen(app.get('port'), () => {
+    console.log('Node.js/Express is listening on port ' + app.get('port'));
+});
+
+// let express = require('express');
+// let app = express();
+// let bodyParser = require('body-parser');
+
+// // middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(express.static('src'));
+
+// app.get('/showtimes', (request,response) => {
+//     response.sendFile(__dirname + '/src/showtimes.html');
+// });
+
+// app.set('port', 3001);
+
+// app.listen(app.get('port'), () => {
+//     console.log('Node.js/Express is listening on port ' + app.get('port'));
+// });
--- src/scripts/client_side.js
+++ src/scripts/client_side.js
@@ -1,3 +1,16 @@
 window.onload = function() {
-    
+    button = document.getElementById('show-info');
+    button.onclick = function() {
+        fetch('http://localhost:3001/showtimes')
+            .then((response) => response.json())
+            .then((showtimes) => {
+            for (let showtime of showtimes) {
+                console.log(showtime.title);
+                // document.getElementById('show-list')
+                //     .setAttribute('value', customer.firstName);
+                // document.getElementById('show-list')
+                //     .setAttribute('value', customer.lastName);
+            }
+            });
+    };
 }
