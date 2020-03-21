bca7f0d2eda22938c8deb96c4239d3d057dc76ac
MatrixPanda <zehan11s@hotmail.com>
2020-03-20 23:14:33 -0400

    Implemented table display and basic links

--- src/scripts/client_side.js
+++ src/scripts/client_side.js
@@ -1,16 +1,49 @@
 window.onload = function() {
-    button = document.getElementById('show-info');
-    button.onclick = function() {
-        fetch('http://localhost:3001/showtimes')
+
+    fetch('http://localhost:3001/showtimes')
             .then((response) => response.json())
             .then((showtimes) => {
-            for (let showtime of showtimes) {
-                console.log(showtime.title);
-                // document.getElementById('show-list')
-                //     .setAttribute('value', customer.firstName);
-                // document.getElementById('show-list')
-                //     .setAttribute('value', customer.lastName);
-            }
+                var tbl = document.createElement("table");
+
+                for (let showtime of showtimes) {
+                    var trow = document.createElement("tr");
+                    for (x=0; x < 2; x++) {
+                        if (x == 0) {
+                            let link = document.createElement('a');
+                            var cellText = document.createTextNode(showtime.title);
+                            // link.setAttribute('href', 'https://www.google.ca/');
+                            // let link = document.createElement('a');
+                            // link.setAttribute('value', showtime.title)  // note: using 'value' doesn't work
+                            link.appendChild(cellText);
+                            link.onclick = function() {
+                                let details = document.getElementById('show-detailed-info');
+                                details.style.display = 'flex';
+                                console.log(showtime.id);
+                            };
+
+                            var tcell = document.createElement("td");
+                            tcell.appendChild(link);
+                            trow.appendChild(tcell);
+                        } else {
+                            // let newDivItem = document.createElement('div');
+                            var newDivItem;
+                            var tcell = document.createElement("td");
+
+                            for (i in showtime.times) {
+                                newDivItem = document.createElement('div');
+                                newDivItem.setAttribute('class', 'times');
+                                var cellText = document.createTextNode(showtime.times[i]);
+                                newDivItem.appendChild(cellText);
+                                tcell.appendChild(newDivItem);
+                            }
+                            trow.appendChild(tcell);
+                        }
+                    }
+                    tbl.appendChild(trow); 
+                }
+
+                let insertToDiv = document.getElementById('show-info');
+                insertToDiv.appendChild(tbl);
             });
-    };
+            
 }
